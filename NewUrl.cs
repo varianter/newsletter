73644b2f47b1afd22030d72fcbe6d4257ebce474
Ole J. Skogstad <skogstad@softis.net>
2020-05-22 23:59:23 +0200

    Create function for posting new url

---
+++ NewUrl.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace newsletter
+{
+    public static class NewUrl
+    {
+        private class NewsletterItem : TableEntity
+        {
+            public string Url { get; set; }
+            public string Description { get; set; }
+            public string Tags  { get; set; }
+        }
+
+        private static bool IsValidUrl(this string url)
+        {
+            if (!Regex.IsMatch(url, @"^https?:\/\/", RegexOptions.IgnoreCase))
+                url = "https://" + url;
+            
+            return Uri.TryCreate(url, UriKind.Absolute, out _);
+        }
+        
+        [FunctionName("NewUrl")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "url/")] HttpRequest req,
+            [Table("NewsletterItems")] CloudTable newsletterItemsTable,
+            ILogger log)
+        {
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var newsletterItem = JsonConvert.DeserializeObject<NewsletterItem>(requestBody);
+
+            if(newsletterItem == null)
+                return new BadRequestResult();
+            
+            if(string.IsNullOrWhiteSpace(newsletterItem.Url) || !newsletterItem.Url.IsValidUrl())
+                return new BadRequestResult();
+
+            newsletterItem.RowKey = Guid.NewGuid().ToString();
+            newsletterItem.PartitionKey = "NewsletterItems";
+            
+            await newsletterItemsTable.ExecuteAsync(TableOperation.Insert(newsletterItem));
+            
+            return new OkObjectResult(newsletterItem);
+        }
+    }
+}
