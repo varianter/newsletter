51da2aa4c5433c5f26540fdea5d43ef626b48dab
Ole J. Skogstad <skogstad@softis.net>
2020-05-23 02:10:53 +0200

    Create function for adding subscriber

---
+++ AddSubscriber.cs
@@ -0,0 +1,49 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+
+namespace newsletter
+{
+    public static class AddSubscriber
+    {
+        private class Subscriber : TableEntity
+        {
+            public string Email { get; set; }
+        }
+
+        private static bool IsValidEmail(this string email) => new EmailAddressAttribute().IsValid(email);
+
+        [FunctionName("AddSubscriber")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "email/")]
+            HttpRequest req,
+            [Table("Subscribers")] CloudTable subscribersTable,
+            ILogger log)
+        {
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var subscriber = JsonConvert.DeserializeObject<Subscriber>(requestBody);
+
+            if (subscriber == null)
+                return new BadRequestResult();
+
+            if (string.IsNullOrWhiteSpace(subscriber.Email) || !subscriber.Email.IsValidEmail())
+                return new BadRequestResult();
+
+            subscriber.RowKey = Guid.NewGuid().ToString();
+            subscriber.PartitionKey = "Subscribers";
+
+            // TODO send verification email
+
+            await subscribersTable.ExecuteAsync(TableOperation.Insert(subscriber));
+            return new OkObjectResult(subscriber);
+        }
+    }
+}
